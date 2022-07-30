34810f7a97bdb472730ce69a33dd303ee56b8c13
Doosies <song961003@gmail.com>
2022-07-30 14:19:15 +0900

    git clean cash

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import MonthSelector from 'components/Common/MonthSelector';
-import React from 'react';
-import logo from './logo.svg';
-
-const App = () => {
-  return <div className="App"></div>;
-};
-
-export default App;
--- src/components/Common/MonthSelector/MonthSelector.style.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledMonthSelector = styled.div`
-  width: 6rem;
-  height: 4rem;
-  background-color: black;
-`;
