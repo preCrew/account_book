849994de8bdf2810f9cb0973b21808b862b4a0cf
Doosies <song961003@gmail.com>
2022-07-30 12:59:43 +0900

    upper

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import App from 'App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'styles/global-style';
+import { theme } from 'styles/theme';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
