15d812a64ecd6a3b464d8bea5192e8278732db74
Doosies <song961003@gmail.com>
2022-07-30 18:11:13 +0900

    add: width, height 100% global

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -4,8 +4,10 @@ import './fonts/fonts.css';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
-  html {
+  html, body, #root {
     font-size: 150%;
+    width: 100%;
+    height: 100%;
   }
   body {
       font-family: appleR;
