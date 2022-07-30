f68a86972a48ba6d1a2b479db21fe5138d7755ba
Doosies <song961003@gmail.com>
2022-07-30 18:12:10 +0900

    delete: test button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import MonthSelector from 'components/Common/MonthSelector';
-import React from 'react';
-
 import Layout from 'components/Common/Layout/Layout';
 import Button from 'components/Common/Button';
 import styled from 'styled-components';
@@ -10,18 +7,8 @@ const App = () => {
     <>
       <Layout>
         내역이 들어가요 메인페이지 에요내역이 들어가요 메인페이지 에요
-        <Button size="small">이건 버튼이다</Button>
-        <Button size="medium">이건 버튼이다</Button>
-        <Button size="large">이건 버튼이다</Button>
-        <Test>
-          <Button size="no">no버튼</Button>
-        </Test>
       </Layout>
     </>
   );
 };
-const Test = styled.div`
-  width: 200px;
-  height: 100px;
-`;
 export default App;
