a8c9a0ee9f37a9b753a5d5a5799eb39aa27f14cc
Doosies <song961003@gmail.com>
2022-08-01 17:20:21 +0900

    delete: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import Layout from 'components/Common/Layout/Layout';
 import MonthSelector from 'components/Common/MonthSelector';
-import CategoryModal from 'components/Common/Modal/ModalComponents/CategoryModal';
-import useModal from 'hooks/useModal';
 
 const App = () => {
-  const { Modal, isOpen, closeModal, openModal } = useModal();
-  const handleClickButton = () => {
-    openModal();
-  };
   return (
     <>
       <Layout>
         내역이 들어가요 메인페이지 에요내역이 들어가요 메인페이지 에요
         <MonthSelector />
-        <button onClick={handleClickButton}>click!</button>
-        <Modal
-          isOpen={isOpen}
-          onClose={closeModal}
-        >
-          <CategoryModal />
-        </Modal>
       </Layout>
     </>
   );
