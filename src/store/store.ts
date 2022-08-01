bc843d80de63db24598a64ae0225bbfdfcd73098
Doosies <song961003@gmail.com>
2022-08-01 15:50:54 +0900

    add: useAppDispatch, useAppSelector

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import accountBookReducer from './accoutBook-Slice';
+import modalReducer from './modal-Slice';
+
+import { useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch } from 'react-redux';
+// import { AppDispatch, RootState } from './reduxStore';
 
 export const store = configureStore({
   reducer: {
     accountBook: accountBookReducer,
+    modal: modalReducer,
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
