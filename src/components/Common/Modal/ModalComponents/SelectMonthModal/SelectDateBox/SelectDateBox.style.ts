552e55fd97d88cc40d6ed3db3d1946287702d32f
Doosies <song961003@gmail.com>
2022-08-04 19:06:30 +0900

    change: selectDateBox style

--- src/components/Common/Modal/ModalComponents/SelectMonthModal/SelectDateBox/SelectDateBox.style.ts
+++ src/components/Common/Modal/ModalComponents/SelectMonthModal/SelectDateBox/SelectDateBox.style.ts
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const StyledSelectDateBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-
   width: 100%;
   height: 100%;
+  display: flex;
 `;
 export const SelectDateButton = styled.div`
+  height: 2.5rem;
   width: 100%;
 `;
