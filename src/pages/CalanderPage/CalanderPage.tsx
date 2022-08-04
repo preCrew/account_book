01c78913e117b0f0fe149dd84d629352871f01de
Doosies <song961003@gmail.com>
2022-08-04 14:13:39 +0900

    change: init firstDate when loading page

--- src/pages/CalanderPage/CalanderPage.tsx
+++ src/pages/CalanderPage/CalanderPage.tsx
@@ -1,7 +1,6 @@
 import SelectMonthModal from 'components/Common/Modal/ModalComponents/SelectMonthModal';
 import useModal from 'hooks/useModal';
-import { useEffect, useMemo } from 'react';
-import useAccountBook from 'store/hooks/useAccountBook';
+import { useMemo } from 'react';
 import { useAppSelector } from 'store/store';
 import { Down100, Up100 } from 'styles/animations';
 import { dateGenerator } from 'utils/dateUtils';
@@ -11,16 +10,21 @@ interface CalanderPageProps {}
 
 const CalanderPage = ({}: CalanderPageProps) => {
   const { Modal, showModal, closeModal } = useModal(Up100, Down100, 300);
+  const firstDateOrigin = useAppSelector(state => state.accountBook.firstDate);
 
   const dates = useMemo(() => {
-    return dateGenerator(20);
-  }, []);
+    const firstDate = new Date(firstDateOrigin.year, firstDateOrigin.month);
+    return dateGenerator(new Date(), firstDate);
+  }, [firstDateOrigin]);
 
   return (
     <>
       <button onClick={showModal}>click!!</button>
       <Modal>
-        <SelectMonthModal dates={dates} />
+        <SelectMonthModal
+          dates={dates}
+          onClose={closeModal}
+        />
       </Modal>
     </>
   );
