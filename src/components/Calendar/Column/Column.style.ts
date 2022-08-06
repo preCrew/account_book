fe110917e15409af08983e0cb2f33cba0cad8132
Doosies <song961003@gmail.com>
2022-08-06 18:43:08 +0900

    add: column Files

---
+++ src/components/Calendar/Column/Column.style.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const StyledColumn = styled.div``;
---
+++ src/components/Calendar/Column/Column.tsx
@@ -0,0 +1,9 @@
+import './Column.style.ts';
+
+interface ColumnProps {}
+
+const Column = ({}: ColumnProps) => {
+  return <></>;
+};
+
+export default Column;
