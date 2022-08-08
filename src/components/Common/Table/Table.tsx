59bd9761fca2c10739a36fb4aeb6f2e5f932d12a
Doosies <song961003@gmail.com>
2022-08-08 17:25:23 +0900

    feat: table component

--- src/components/Common/Table/Table.tsx
+++ src/components/Common/Table/Table.tsx
@@ -1,9 +1,40 @@
-import './Table.style.ts';
+import React from 'react';
 
-interface TableProps {}
+export interface TTableData {
+  id: string;
+  columns: {
+    render: React.ReactNode;
+    key: string;
+  }[];
+}
+interface TableProps {
+  headers: string[];
+  rows: TTableData[];
+}
 
-const Table = ({}: TableProps) => {
-  return <></>;
+const Table = ({ headers, rows }: TableProps) => {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            {headers.map(header => (
+              <th key={`T_${header}`}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map(({ columns, id }) => (
+            <tr key={id}>
+              {columns.map(({ key, render }) => (
+                <td key={key}>{render}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Table;
