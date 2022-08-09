d089826e2e824598e17fce50b2cdfbaedaa0a5c2
이챙 <cycy8527@gmail.com>
2022-08-09 15:37:20 +0900

    Fix: layout style
    - 컨텐츠 마진값 추가
    - 불필요한 코드 삭제

--- src/components/Common/Layout/Layout.style.ts
+++ src/components/Common/Layout/Layout.style.ts
@@ -2,20 +2,15 @@ import styled from 'styled-components';
 
 export const navHeightVal = `${40}px`;
 export const contentsMinHeight = `${540}px`;
+export const contentsMargin = `${15}px`;
 
 export const LayoutWrap = styled.div`
   height: 100%;
   max-width: ${({ theme }) => theme.deviceSizes.mobile};
   margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
 `;
 
 export const ContentsBox = styled.section`
   min-height: ${contentsMinHeight};
-  padding: 0 20px ${navHeightVal};
-  height: calc(100% - 40px);
-  height: -webkit-calc(100% - 40px); /* for Chrome, Safari */
-  height: -moz-calc(100% - 40px); /* for Firefox */
+  padding: 0 ${contentsMargin} ${navHeightVal};
 `;
