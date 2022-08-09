be8bd5a9dc8600e5707e7aaa2d93f54888b63902
Doosies <song961003@gmail.com>
2022-08-09 15:19:00 +0900

    change: call component

--- src/pages/CalendarPage/CalendarPage.tsx
+++ src/pages/CalendarPage/CalendarPage.tsx
@@ -1,46 +1,21 @@
+import Calendar from 'components/Calendar';
 import MonthSelector from 'components/Common/MonthSelector';
-import Table from 'components/Common/Table';
-import { TTableData } from 'components/Common/Table/Table';
+import { useAppSelector } from 'store/store';
 import './CalendarPage.style.ts';
 
 interface CalendarPageProps {}
 
 const CalendarPage = ({}: CalendarPageProps) => {
-  const data: TTableData[] = [
-    {
-      id: '첫번째',
-      columns: [
-        {
-          key: '1',
-          render: <div>111</div>,
-        },
-        {
-          key: '2',
-          render: <div>2222</div>,
-        },
-      ],
-    },
-    {
-      id: '2',
-      columns: [
-        {
-          key: '4',
-          render: <div>444</div>,
-        },
-        {
-          key: '5',
-          render: <div>555</div>,
-        },
-      ],
-    },
-  ];
-  const titles = ['월', '화'];
+  // const date = calendarDataGenerator(new Date());
+  const { year, month } = useAppSelector(state => state.accountBook.selectDate);
+  // console.log(date
+  // console.log(date.getDay());
   return (
     <>
       <MonthSelector />
-      <Table
-        rows={data}
-        headers={titles}
+      <Calendar
+        year={year}
+        month={month}
       />
     </>
   );
