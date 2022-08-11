a66b24b458ba40f62eecf45c7e6d436e77e8ed9a
Doosies <song961003@gmail.com>
2022-08-12 01:06:39 +0900

    test: delete test code

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,8 @@ import Mypage from 'pages/MyPage';
 
 import useAccountBook from 'store/hooks/useAccountBook';
 
-import { auth } from 'firebaseConfig';
-import useUser from 'store/hooks/useUser';
-
 const App = () => {
-  console.log(auth.currentUser);
   const { changeSelectDate } = useAccountBook();
-  const { loginUser, createUser, logoutUser } = useUser();
 
   useEffect(() => {
     const nowDate = new Date();
@@ -25,29 +20,10 @@ const App = () => {
     changeSelectDate({ year, month });
   }, []);
 
-  const handle = () => {
-    loginUser('google');
-  };
-  const handle2 = () => {
-    logoutUser();
-  };
-  const handle3 = async () => {
-    createUser('song96101ssffffff2303@sbs.ser', '124434');
-  };
-  const handle4 = async () => {
-    loginUser('idpw', {
-      email: 'song96101ssffffff2303@sbs.ser',
-      password: '124434',
-    });
-  };
   return (
     <>
       <Layout>
-        <button onClick={handle}>google</button>
-        <button onClick={handle2}>logout</button>
-        <button onClick={handle3}>signUp</button>
-        <button onClick={handle4}>login</button>
-        {/* <Routes>
+        <Routes>
           <Route
             path="/"
             element={<Home />}
@@ -64,7 +40,7 @@ const App = () => {
             path="/mypage"
             element={<Mypage />}
           />
-        </Routes> */}
+        </Routes>
       </Layout>
     </>
   );
