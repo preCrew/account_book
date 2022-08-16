87f4a4f22d89b9fa08f4eaf79273ec75d8e64efa
Doosies <song961003@gmail.com>
2022-08-16 12:54:16 +0900

    Test: delete test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from 'components/Common/Layout/Layout';
@@ -9,45 +9,17 @@ import Mypage from 'pages/MyPage';
 
 import useAccountBook from 'store/hooks/useAccountBook';
 
-import { app, auth, db } from 'firebaseConfig';
+import { auth } from 'firebaseConfig';
 import useUser from 'store/hooks/useUser';
-import { useAppSelector } from 'store/store';
-import {
-  addDoc,
-  arrayUnion,
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  serverTimestamp,
-  setDoc,
-  snapshotEqual,
-  Timestamp,
-  updateDoc,
-  where,
-} from 'firebase/firestore';
-import { TReceipt } from 'store/reducers/accoutBook-Slice';
-import { getAuth } from 'firebase/auth';
 
 const App = () => {
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-  });
-  const { changeSelectDate } = useAccountBook();
-  const { loginUser, createUser, logoutUser, keepingLoginState } = useUser();
-  // const { email, uid } = useAppSelector(state => state.user);
-  const { addReceipt, readReceipts, deleteReceipt, updateReceipt } =
-    useAccountBook();
+  const { keepingLoginState } = useUser();
+  const { readReceipts } = useAccountBook();
 
   useEffect(() => {
-    // const a = getAuth();
-    // console.log(auth.currentUser);
     const nowDate = new Date();
     const year = nowDate.getFullYear();
     const month = nowDate.getMonth() + 1;
-    // changeSelectDate({ year, month });
 
     // 만약 로그인된 상태면 새로고침 or 페이지 이동돼도 로그인 유지
     auth.onAuthStateChanged(user => {
@@ -57,88 +29,9 @@ const App = () => {
       }
     });
   }, []);
-  console.log(getAuth().currentUser);
-
-  const handleChangeUser = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUser(state => ({
-      ...state,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleDelete = () => {
-    deleteReceipt(1660555924);
-  };
-  const handleUpdate = () => {
-    updateReceipt({
-      id: 1660555924,
-      receiptNew: {
-        spending: 0,
-        income: 1124142,
-      },
-    });
-  };
-
-  const loginGoogle = () => {
-    loginUser('google');
-  };
-  const logout = () => {
-    logoutUser();
-  };
-  const signUpUser = async () => {
-    createUser(user.email, user.password);
-  };
-  const loginUserHandler = async () => {
-    loginUser('idpw', {
-      email: user.email,
-      password: user.password,
-    });
-  };
-  // 영수증 추가
-  const handleDB = async () => {
-    // const date = new Date();
-    // date.setMonth(date.getMonth() + 1);
-
-    const receipt: TReceipt = {
-      category: '놀았어',
-      account: '피시방',
-      spending: -1000,
-      // income: 1000,
-      memo: 'test Memo',
-      timeDate: {
-        year: 2022,
-        month: 8,
-        date: 14,
-        hours: 12,
-        minutes: 25,
-      },
-    };
-    addReceipt(receipt);
-  };
-  const handleGetDB = async () => {
-    readReceipts({ year: 2022, month: 8 });
-  };
   return (
     <>
       <Layout>
-        <input
-          name="email"
-          type="name"
-          onChange={handleChangeUser}
-        />
-        <input
-          name="password"
-          type="text"
-          onChange={handleChangeUser}
-        />
-        <button onClick={loginGoogle}>google</button>
-        <button onClick={logout}>logout</button>
-        <button onClick={signUpUser}>signUp</button>
-        <button onClick={loginUserHandler}>login</button>
-        <button onClick={handleDB}>DB</button>
-        <button onClick={handleGetDB}>getDB</button>
-        <button onClick={handleDelete}>deleteDB</button>
-        <button onClick={handleUpdate}>updateDB</button>
         <Routes>
           <Route
             path="/"
