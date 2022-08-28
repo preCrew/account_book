96e64943c05a40b28a78f64e6a094ce4dfac035b
ChoiSunAh, 최선아 <csa2676@naver.com>
2022-08-29 01:40:34 +0900

    feat:UserName, BudgetSave

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useAppDispatch from '../../store/hooks/useAccountBook';
 import { useAppSelector } from '../../store/store';
 import Input from 'components/Common/Input';
@@ -24,6 +24,7 @@ import {
 
 import AvatarImg from 'components/Profile/Avatar/AvatarImg';
 import useUser from 'store/hooks/useUser';
+import useInput from 'hooks/useInput';
 
 // interface MyPageProps {}
 
@@ -33,24 +34,43 @@ const MyPage = () => {
   const { logoutUser } = useUser();
   const MAX_AMOUNT = 9999999999;
 
-  const dispatch = useAppDispatch();
-  const accountBook = useAppSelector(state => state.accountBook);
   const user = useAppSelector(state => state.user);
   const { Modal, showModal, closeModal } = useModal(Up100, Down100, 300);
+  const [value, setValue, onChange] = useInput('Name');
+  const [amount, setAmount, onChangeAmount] = useInput(0);
+  const { getUserInfo } = useUser();
+  const { updateUserInfo, changeBudget, changeName } = useUser();
+
+  useEffect(() => {
+    getUserInfo();
+    if (user.userInfo.name !== '') {
+      setValue(user.userInfo.name);
+      setAmount(user.userInfo.budget);
+    }
+  }, []);
 
   const handleChangeAmountSetting = (e: InputChangeEvent) => {
-    let amount = e.target.valueAsNumber;
+    setAmount(e.target.valueAsNumber);
+
     //Amount유효성 검사
-    if (amount) {
+    if (amount > -1) {
       if (amount > MAX_AMOUNT || amount < 0) {
         alert('입력할 수 있는 단위가 아닙니다.');
-        amount = NaN;
-      } else {
-        dispatch.changeAmount(amount);
+        setAmount(NaN);
       }
     } else {
-      dispatch.changeAmount(0);
+      setAmount(0);
     }
+  };
+
+  const handleClickSave = () => {
+    changeName(value as string);
+    changeBudget(amount as number);
+    updateUserInfo();
+
+    getUserInfo();
+    setValue(user.userInfo.name);
+    setAmount(user.userInfo.budget);
   };
   const handleClickLogout = () => {
     logoutUser();
@@ -66,7 +86,13 @@ const MyPage = () => {
           <Profile onClick={showModal}>
             <AvatarImg size="100" />
           </Profile>
-          <Name>Name</Name>
+          <Name>
+            <Input
+              type={'text'}
+              value={value}
+              onChange={onChange}
+            />
+          </Name>
           <Emaile>{user.email}</Emaile>
           <SubTitle>예산을 입력해주세요</SubTitle>
           <AmountBox>
@@ -74,12 +100,20 @@ const MyPage = () => {
             <AmountInputBox>
               <Input
                 type={'number'}
-                value={accountBook.amount}
-                onChange={handleChangeAmountSetting}
+                value={amount}
+                onChange={e => {
+                  onChangeAmount(e);
+                  handleChangeAmountSetting(e);
+                }}
               />
             </AmountInputBox>
           </AmountBox>
-          <SaveButton size="full">저장하기</SaveButton>
+          <SaveButton
+            size="full"
+            onClick={handleClickSave}
+          >
+            저장하기
+          </SaveButton>
           <LogoutBtn
             type="button"
             onClick={handleClickLogout}
